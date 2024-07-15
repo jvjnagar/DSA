b7f68eca9c7f9531316194252e2c1c7df59f1747
jvjnagar <51321550+jvjnagar@users.noreply.github.com>
2024-07-15 09:08:00 +0530

    Update Next_smaller_element_4_eachElement.cpp

--- Stack/Next_smaller_element_4_eachElement.cpp
+++ Stack/Next_smaller_element_4_eachElement.cpp
@@ -1,4 +1,6 @@
-   
+#include <bits/stdc++.h>   
+using namespace std;
+
 //brute-Force approach
 vector<int> findNextSmaller(vector<int> arr, int n) 
     {
@@ -19,3 +21,44 @@ vector<int> findNextSmaller(vector<int> arr, int n)
         }
         return nextElements;
     } 
+
+//Using Stack
+    vector<int> findNextSmallerStack(vector<int> arr, int n) 
+    {
+        vector<int> nextElements(n);
+        int next;
+        stack<int> s;
+        for(int i=n-1; i>=0; i--)
+        {
+            next=-1;
+            while( (!s.empty()) && (s.top()>=arr[i])) s.pop();
+            nextElements[i] = s.empty() ? -1 : s.top();
+            s.push(arr[i]);
+        }
+        return nextElements;
+    } 
+
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.findNextSmallerStack(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0; 
+}
