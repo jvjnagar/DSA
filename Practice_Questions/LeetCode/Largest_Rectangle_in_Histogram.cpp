dfe412e4d43bce3b8bb5472a88d83eef3c3a8611
jvjnagar <51321550+jvjnagar@users.noreply.github.com>
2024-07-15 21:40:04 +0530

    Update Largest_Rectangle_in_Histogram.cpp

--- Practice_Questions/LeetCode/Largest_Rectangle_in_Histogram.cpp
+++ Practice_Questions/LeetCode/Largest_Rectangle_in_Histogram.cpp
@@ -1,11 +1,64 @@
+#include <bits/stdc++.h>   
+using namespace std;
+
 class Solution {
 public:
+    vector<int> findNextSmaller(vector<int> arr, int n) 
+    {
+        vector<int> nextSmallerElements(n);
+        int next;
+        stack<int> s;
+        for(int i=n-1; i>=0; i--)
+        {
+            while( (!s.empty()) && (arr[s.top()]>=arr[i])) s.pop();
+            nextSmallerElements[i] = s.empty() ? n : s.top();
+            s.push(i);
+        }
+        return nextSmallerElements;
+    } 
+
+    vector<int> findPrevSmaller(vector<int> arr, int n) 
+    {
+        vector<int> prevSmallerElements(n);
+        int next;
+        stack<int> s;
+        for(int i=0; i<n; i++)
+        {
+            while( (!s.empty()) && (arr[s.top()]>=arr[i])) s.pop();
+            prevSmallerElements[i] = s.empty() ? -1 : s.top();
+            s.push(i);
+        }
+        return prevSmallerElements;
+    } 
+
     int largestRectangleArea(vector<int>& heights) {
         int i;
-        int length = heights.size();
-        for(i=0; i<length; i++)
+        int n = heights.size();
+        int maxArea = 0;
+        int area;
+        vector<int> nextSmaller(n);
+        vector<int> prevSmaller(n);
+        
+        nextSmaller = findNextSmaller(heights, n);
+        prevSmaller = findPrevSmaller(heights, n);
+
+        for(i=0; i<n; i++)
         {
-            
-        }
+            area = heights[i]*(nextSmaller[i]-prevSmaller[i]-1);
+            maxArea = max(maxArea, area);
+        }        
+        return maxArea;
     }
+    
 };
+
+// Driver code 
+int main() 
+{ 
+    vector<int> height = { 2,1,5,6,2,3 }; 
+    Solution sln;
+  
+    // Function call 
+    cout << "Maximum area is " << sln.largestRectangleArea(height); 
+    return 0; 
+}
