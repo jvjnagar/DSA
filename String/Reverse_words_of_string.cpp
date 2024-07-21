fa185ffc927443c3e87d6f212fa3f644aeb9c36f
jvjnagar <51321550+jvjnagar@users.noreply.github.com>
2024-07-21 21:51:32 +0530

    Update Reverse_words_of_string.cpp

--- String/Reverse_words_of_string.cpp
+++ String/Reverse_words_of_string.cpp
@@ -8,6 +8,36 @@ class Solution
 {
     public:
     //Function to reverse words in a given string.
+    //Time complexity: O(N), Auxiliary Space: O(1)
+    string reverse_words(string s)
+    {
+        int left = 0, i = 0, n = s.size();
+    
+        while (s[i] == ' ')
+            i++;
+    
+        left = i;
+    
+        while (i < n) {
+            if (i + 1 == n || s[i] == ' ') {
+                int j = i - 1;
+                if (i + 1 == n)
+                    j++;
+    
+                while (left < j)
+                    swap(s[left++], s[j--]);
+    
+                left = i + 1;
+            }
+            if (i > left && s[left] == ' ')
+                left = i;
+    
+            i++;
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    }
+    //Time complexity: O(N), Auxiliary Space: O(N)
     string reverseWords(string S) 
     { 
         int i, j, k, l;
