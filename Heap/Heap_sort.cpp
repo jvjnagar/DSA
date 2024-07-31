e5f6e06612b0546059e8ac4189b0954faa6955ec
jvjnagar <51321550+jvjnagar@users.noreply.github.com>
2024-07-31 08:14:30 +0530

    Update Heap_sort.cpp

--- Heap/Heap_sort.cpp
+++ Heap/Heap_sort.cpp
@@ -5,17 +5,17 @@ void insert(int A[], int n)
 {
     int temp, i=n;
     temp = A[i];
-    while(i>1 && A[i/2]<temp)
+    while(i>=1 && A[(i-1)/2]<temp)
     {
-        A[i] = A[i/2];
-        i = i/2;
+        A[i] = A[(i-1)/2];
+        i = (i-1)/2;
     }
     A[i] = temp;
 }
 
 void createHeap(int A[], int n)
 {
-    for(int i = 2; i<=n; i++ )
+    for(int i = 1; i<=n; i++ )
     {
         insert(A, i);
     }
@@ -24,12 +24,12 @@ void createHeap(int A[], int n)
 
 void Delete(int A[], int n)
 {
-    int i=1, j;
+    int i=0, j;
     int temp, temp2;
-    temp = A[1];
+    temp = A[0];
     A[i] = A[n];
-    j = i*2;
-    while(j<n-1)
+    j = i*2+1;
+    while(j<n)
     {
         if(A[j]<A[j+1])
             j = j+1;
@@ -39,7 +39,7 @@ void Delete(int A[], int n)
             A[i] = A[j];
             A[j] = temp2;
             i=j;
-            j= j*2;
+            j= j*2+1;
         }
         else
             break;
@@ -50,19 +50,19 @@ void Delete(int A[], int n)
 void heapSort(int A[], int n)
 {
     int i;
-    for(i=n;i>1; i--)
+    for(i=n-1;i>=1; i--)
     {
         Delete(A, i);
     }
 }
 int main()
 {
-    int A[] = {0,40,35,15,30,10,12,6,5,20,50};
+    int A[] = {40,35,15,30,10,12,6,5,20,50};
     createHeap(A, 10);
     
     heapSort(A, 10);
     
-    for(int i=0; i<11; i++)
+    for(int i=0; i<10; i++)
         printf("%d, ", A[i]);
     printf("\n");
     
