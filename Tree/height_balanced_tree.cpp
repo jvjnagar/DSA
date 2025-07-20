93f902989db8ed28e67b21ff6cf7d0200f46c617
jvjnagar <51321550+jvjnagar@users.noreply.github.com>
2025-07-20 17:14:07 +0530

    Update height_balanced_tree.cpp

--- Tree/height_balanced_tree.cpp
+++ Tree/height_balanced_tree.cpp
@@ -1,3 +1,21 @@
+/*Given a binary tree, determine if it is height-balanced.
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+    
+Example 2:
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+    
+Example 3:
+Input: root = []
+Output: true
+
+Example 4:
+root = [1,2,3,4,5,6,null,8]
+Output: true
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
